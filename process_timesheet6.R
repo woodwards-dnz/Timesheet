e1a78a106570e1dbf2a4e7c66868488c47ed4093
woodwards-dnz <simon.woodward@dairy.co.nz>
2023-08-21 08:08:33 +1200

    Commit old method.

--- process_timesheet6.R
+++ process_timesheet6.R
@@ -19,7 +19,7 @@ Sys.sleep(1)
 # options ####
 options(dplyr.summarise.inform = FALSE)
 path <- "Timesheet2023.xlsx"
-done <- ymd("2023-05-29") # monday
+done <- ymd("2023-07-31") # monday
 print(paste("Done to", done))
 wdays <- wday(done + days(0:6), week_start = 1, label = TRUE, abbr = TRUE)
 
